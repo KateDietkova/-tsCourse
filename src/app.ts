16d2c0e714c0ebf9a0f81ff34c5798eacc1b2602
Katerina Dietkova <katrinkir1296@gmail.com>
2023-06-18 19:12:22 +0300

    add practise 5

--- src/app.ts
+++ src/app.ts
@@ -164,4 +164,59 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
+
 // Practise 5
+
+//task 1
+function getPromise(): Promise<Array<string | number>> {
+  return new Promise((resolve) => {
+    resolve(["Text", 50]);
+  });
+}
+
+getPromise().then((data) => {
+  console.log(data);
+});
+
+//task 2
+
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
+
+function compare(
+  top: Pick<AllType, "name" | "color">,
+  bottom: Pick<AllType, "position" | "weight">
+): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
+
+//task 3
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+//task 4
+
+class Component<T> {
+  constructor(public props: T) {}
+}
+
+interface IProps {
+  title: string;
+}
+
+class Page extends Component<IProps> {
+  pageInfo() {
+    console.log(this.props.title);
+  }
+}
